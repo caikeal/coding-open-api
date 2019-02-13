cad69c570b2826f43c522ebfeb5a32c4d0938963
朱正钱 <hachi.zzq@gmail.com>
2019-02-13 14:10:56 +0800

    新增当前用户在企业的角色列表接口

--- src/Enterprise/User/Client.php
+++ src/Enterprise/User/Client.php
@@ -60,4 +60,15 @@ class Client extends BaseClient
     {
         return $this->httpGet('/api/account/key/'.$username);
     }
+
+    /**
+     * 当前登录人在企业下的角色列表
+     * @return array|\CodingOpenApi\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \CodingOpenApi\Kernel\Exceptions\InvalidConfigException
+     * @author: hachi.zzq <hachi.zzq@gmail.com>
+     */
+    public function currentEnterpriseRoles()
+    {
+        return $this->httpGet('/api/team/'.$this->app['config']['team'].'/current-user/roles');
+    }
 }
